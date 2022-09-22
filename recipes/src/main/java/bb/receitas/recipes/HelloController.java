527bba8c619bd72a5b28366a8cf4c6a686f193f4
Kaiaffa <pcaiafamarques@gmail.com>
2022-09-22 18:53:22 -0300

    Iniciando spring

---
+++ recipes/src/main/java/bb/receitas/recipes/HelloController.java
@@ -0,0 +1,13 @@
+package bb.receitas.recipes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "teste";
+    }
+}
