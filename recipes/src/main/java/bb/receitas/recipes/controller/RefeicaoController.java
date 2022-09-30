281ce7d86630b4b60fe001b6025d55cde8246a18
Kaiaffa <pcaiafamarques@gmail.com>
2022-09-30 19:24:55 -0300

    Novo point refeicoes

--- recipes/src/main/java/bb/receitas/recipes/controller/RefeicaoController.java
+++ recipes/src/main/java/bb/receitas/recipes/controller/RefeicaoController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bb.receitas.recipes.entity.Autor;
+import bb.receitas.recipes.entity.Receita;
 import bb.receitas.recipes.entity.Refeicao;
+import bb.receitas.recipes.service.AutorService;
+import bb.receitas.recipes.service.ReceitaService;
 import bb.receitas.recipes.service.RefeicaoService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
@@ -19,9 +23,13 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RequestMapping("/refeicao")
 public class RefeicaoController {
     final RefeicaoService refeicaoService;
+    final ReceitaService receitaService;
+    final AutorService autorService;
 
-    public RefeicaoController(RefeicaoService refeicaoService){
+    public RefeicaoController(RefeicaoService refeicaoService, ReceitaService receitaService, AutorService autorService){
         this.refeicaoService = refeicaoService;
+        this.receitaService = receitaService;
+        this.autorService = autorService;
     }
 
     @GetMapping
@@ -50,7 +58,7 @@ public class RefeicaoController {
     }
 
     @DeleteMapping("/remover")
-    public ResponseEntity<Refeicao> removerRefei(@RequestBody Refeicao refeicao) {
+    public ResponseEntity<Refeicao> removerRefeicao(@RequestBody Refeicao refeicao) {
         return ResponseEntity.ok(refeicaoService.remove(refeicao));
     }
 
@@ -67,5 +75,20 @@ public class RefeicaoController {
     @GetMapping("/sobremesa/{receita}")
     public ResponseEntity<List<Refeicao>> refeicoesPorSobremesa(@PathVariable(value = "receita") String sobremesa) {
         return ResponseEntity.ok(refeicaoService.filtradoPorSobremesa(sobremesa));
+    }
+
+    @PostMapping("/{autorId}/{entradaId}/{principalId}/{sobremesaId}")
+    public ResponseEntity<Refeicao> novaRefeicaoNomesReceitas(@RequestBody Refeicao refeicao, @PathVariable(value = "autorId") Long autorId,
+                                    @PathVariable(value = "entradaId") Long entradaId, @PathVariable(value = "principalId") Long principalId,
+                                    @PathVariable(value = "sobremesaId") Long sobremesaId ) {
+        Autor autor = autorService.getById(autorId);
+        Receita entradaRefeicao = receitaService.getById(entradaId);
+        Receita principalRefeicao = receitaService.getById(principalId);
+        Receita sobremesaRefeicao = receitaService.getById(sobremesaId);
+        refeicao.setAutor(autor);
+        refeicao.setEntrada(entradaRefeicao);
+        refeicao.setPrincipal(principalRefeicao);
+        refeicao.setSobremesa(sobremesaRefeicao);
+        return ResponseEntity.ok(refeicaoService.save(refeicao));
     }
 }
